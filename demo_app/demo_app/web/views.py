34f4cca3ef51f407e0276abd352a1c2233656441
Albena Yordanova <95187307+albenayordanova@users.noreply.github.com>
2022-05-09 10:43:29 +0300

    Added a model and simple list view

--- demo_app/demo_app/web/views.py
+++ demo_app/demo_app/web/views.py
@@ -1,3 +1,9 @@
+from django.views import generic as view
 from django.shortcuts import render
 
-# Create your views here.
+from demo_app.web.models import Item
+
+
+class IndexView(view.ListView):
+    template_name = 'index.html'
+    model = Item
